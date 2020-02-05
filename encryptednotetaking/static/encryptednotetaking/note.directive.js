ceb9d6d767ccce08ff3240d9259aa7fb6b43ce36
SeanMaclochlainn <seanmcloughlin9@gmail.com>
2020-02-05 23:05:20 +0000

    added edit and delete functionality

--- encryptednotetaking/static/encryptednotetaking/note.directive.js
+++ encryptednotetaking/static/encryptednotetaking/note.directive.js
@@ -7,7 +7,33 @@
         function NoteDirective(){
             return {
                 templateUrl: 'static/encryptednotetaking/note.html',
-                restrict: 'E'
+                restrict: 'E',
+                controller: ['$scope', '$http', function ($scope, $http) {
+                    var url = '/encryptednotetaking/notes/' + $scope.note.id + '/';
+                    $scope.update = function () {
+                        $http.put(
+                            url,
+                            $scope.note
+                        );
+                    };
+
+                    $scope.delete = function(){
+                        $http.delete(url).then(
+                            function(){
+                                var notes = $scope.data;
+                                notes.splice(
+                                    notes.indexOf($scope.note),
+                                    1
+                                );
+                            }
+                        );
+                    };
+
+                    $scope.modelOptions = {
+                        debounce: 500
+                    };
+                }]
+    
             };
         }
 }());
