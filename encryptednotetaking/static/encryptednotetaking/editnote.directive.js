115b9961ddec4933a1de771b7939d66c75dc7a19
SeanMaclochlainn <seanmcloughlin9@gmail.com>
2020-02-06 20:32:05 +0000

    added modal to edit and new note

--- encryptednotetaking/static/encryptednotetaking/editnote.directive.js
+++ encryptednotetaking/static/encryptednotetaking/editnote.directive.js
@@ -9,7 +9,7 @@
                 templateUrl: 'static/encryptednotetaking/editnote.html',
                 restrict: 'E',
                 controller: ['$scope', '$http', function ($scope, $http) {
-                    var url = '/encryptednotetaking/notes/' + $scope.note.id + '/';
+                    let url = '/encryptednotetaking/notes/' + $scope.note.id + '/';
                     $scope.update = function () {
                         $http.put(
                             url,
@@ -20,7 +20,7 @@
                     $scope.delete = function(){
                         $http.delete(url).then(
                             function(){
-                                var notes = $scope.data;
+                                let notes = $scope.data;
                                 notes.splice(
                                     notes.indexOf($scope.note),
                                     1
--- encryptednotetaking/static/encryptednotetaking/newnote.directive.js
+++ encryptednotetaking/static/encryptednotetaking/newnote.directive.js
@@ -9,15 +9,11 @@
                 templateUrl: 'static/encryptednotetaking/newnote.html',
                 restrict: 'E',
                 controller: ['$scope', '$http', function ($scope, $http) {
-                    $scope.add = function(title, content){
-                        let note = {
-                            title: title,
-                            encrypted_content: content
-                        };
-                        $http.post('/encryptednotetaking/notes/', note).then(
+                    $scope.add = function(){
+                        $http.post('/encryptednotetaking/notes/', $scope.note).then(
                             function(){
-                                var notes = $scope.data;
-                                notes.push(note);
+                                let notes = $scope.data;
+                                notes.push($scope.note);
                                 $scope.isNewNoteActive = false;
                             }
                         );
